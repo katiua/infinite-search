dd4b9ada3e5cd8a88149b9f41c85157d9ca84277
Katiua <>
2026-07-05 11:31:16 +0800

    chore: update manifest

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,8 @@ import pkg from './package.json'
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: 'Infinite Search',
+  description: pkg.description,
   version: pkg.version,
   icons: {
     48: 'public/logo.png',
